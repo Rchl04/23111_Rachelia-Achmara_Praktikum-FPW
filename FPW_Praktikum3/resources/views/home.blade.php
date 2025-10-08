7f2cf61e85e65c83837e41324f7bb0806f8f1c1d
Rchl04 <racheliaachmara@gmail.com>
2025-10-08 21:38:32 +0700

    FPW Pertemuan 1-5

---
+++ FPW_Praktikum3/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('master')
+
+@section('title', 'Home Page')
+
+@section('content')
+    <h1>Welcome to the Home Page, Rachel</h1>
+@endsection
---
+++ FPW_Praktikum3/resources/views/layout-percobaan/app.blade.php
@@ -0,0 +1,7 @@
+@extends('master')
+
+@section('title', 'Home Page')
+
+@section('content')
+    <h1>tralala lelo tralala</h1>
+@endsection
